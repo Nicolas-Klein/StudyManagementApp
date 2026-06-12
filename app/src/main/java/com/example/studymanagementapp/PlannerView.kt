d532871f595a0d2782b1b05bf4eca42771322dbf
Nicolas Klein <100135702+Nicolas-Klein@users.noreply.github.com>
2026-06-12 02:39:17 +0200

    added input dialog for tasks and deadlines

--- app/src/main/java/com/example/studymanagementapp/PlannerView.kt
+++ app/src/main/java/com/example/studymanagementapp/PlannerView.kt
@@ -1,29 +1,28 @@
 package com.example.studymanagementapp
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 
@@ -47,6 +46,62 @@ fun PlannerScreen() {
     var selectedTabIndex by remember { mutableStateOf(Tabs.WOCHE) }
 
 
+    var showDialog by remember { mutableStateOf(false) }
+
+    var taskTitleInput by remember { mutableStateOf("") }
+    var deadlineDateInput by remember { mutableStateOf("") }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = {
+                Text(text = if (selectedTabIndex == Tabs.WOCHE) "Neue Aufgabe hinzufügen" else "Neue Deadline hinzufügen")
+            },
+            text = {
+                Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                    OutlinedTextField(
+                        value = taskTitleInput,
+                        onValueChange = { taskTitleInput = it },
+                        label = { Text("Titel / Beschreibung" ) },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    if (selectedTabIndex == Tabs.MONAT) {
+                        OutlinedTextField(
+                            value = deadlineDateInput,
+                            onValueChange = { deadlineDateInput = it },
+                            label = { Text("Fälligkeitsdatum") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                }
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        if (selectedTabIndex == Tabs.WOCHE){
+                            println("Speicher Wochenaufgabe: $taskTitleInput")
+                        } else {
+                            println("Speicher Deadline: $taskTitleInput am $deadlineDateInput")
+                        }
+
+                        taskTitleInput = ""
+                        deadlineDateInput = ""
+                        showDialog = false
+                    }
+                ) {
+                    Text("Speichern")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text("Abbrechen")
+                }
+            }
+        )
+    }
+
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -55,6 +110,8 @@ fun PlannerScreen() {
                         Tabs.WOCHE -> println("Woche")
                         Tabs.MONAT -> println("Monat")
                     }
+
+                    showDialog = true
                 },
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
                 contentColor = MaterialTheme.colorScheme.onPrimaryContainer
