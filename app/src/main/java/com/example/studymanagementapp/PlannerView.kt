877cc8590047be5fa6d3ce1986bfbb1dda4e5388
Nicolas Klein <100135702+Nicolas-Klein@users.noreply.github.com>
2026-06-12 02:26:24 +0200

    added top layer scaffolding

--- app/src/main/java/com/example/studymanagementapp/PlannerView.kt
+++ app/src/main/java/com/example/studymanagementapp/PlannerView.kt
@@ -3,13 +3,16 @@ package com.example.studymanagementapp
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -43,84 +46,48 @@ enum class Weekdays {
 fun PlannerScreen() {
     var selectedTabIndex by remember { mutableStateOf(Tabs.WOCHE) }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        TabRow(selectedTabIndex = selectedTabIndex.ordinal) {
-            Tabs.entries.forEachIndexed { index, title ->
-                Tab(
-                    selected = selectedTabIndex.ordinal == index,
-                    onClick = { selectedTabIndex = title },
-                    text = { Text(text = title.toString()) }
-                )
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    when (selectedTabIndex){
+                        Tabs.WOCHE -> println("Woche")
+                        Tabs.MONAT -> println("Monat")
+                    }
+                },
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
             }
-        }
-
-        when(selectedTabIndex) {
-            Tabs.WOCHE -> WeekView()
-            Tabs.MONAT -> MonthView()
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun WeekView() {
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        var state by remember { mutableStateOf(Weekdays.MO) }
-        listOf("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag")
-        Column {
-            SecondaryTabRow(selectedTabIndex = state.ordinal) {
-                Weekdays.entries.forEachIndexed { index, title ->
+        },
+        topBar = {
+            TabRow(selectedTabIndex = selectedTabIndex.ordinal) {
+                Tabs.entries.forEachIndexed { index, title ->
                     Tab(
-                        selected = state.ordinal == index,
-                        onClick = { state = title },
-                        text = { Text(text = title.toString(), maxLines = 2, overflow = TextOverflow.Ellipsis) },
+                        selected = selectedTabIndex.ordinal == index,
+                        onClick = { selectedTabIndex = title },
+                        text = { Text(text = title.toString()) }
                     )
                 }
             }
+        }
 
-            when(state) {
-                Weekdays.MO -> WeekdayView(state)
-                Weekdays.DI -> WeekdayView(state)
-                Weekdays.MI -> WeekdayView(state)
-                Weekdays.DO -> WeekdayView(state)
-                Weekdays.FR -> WeekdayView(state)
-                Weekdays.SA -> WeekdayView(state)
-                Weekdays.SO -> WeekdayView(state)
+    ) { innerPadding ->
+
+        Column(modifier = Modifier.fillMaxSize()) {
+
+
+            when(selectedTabIndex) {
+                Tabs.WOCHE -> WeekView(innerPadding)
+                Tabs.MONAT -> MonthView(innerPadding)
             }
         }
-    }
-}
-
-@Composable
-fun MonthView() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = androidx.compose.ui.Alignment.Center
-    ) {
-        Text("Hier entsteht der Monatsplan!", style = MaterialTheme.typography.headlineMedium)
-
 
     }
+
+
+
 }
 
-@Composable
-fun WeekdayView(weekday: Weekdays) {
-
-    var counter = 0;
-
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(weekday.toString(), style = MaterialTheme.typography.headlineMedium)
-        Text(counter.toString())
-
-        FloatingActionButton(
-            onClick = { counter++ },
-            modifier = Modifier.offset(30.dp, 30.dp)
-        ) {
-            Icon(Icons.Filled.Add, "Floating Action Button.")
-        }
-    }
-}
