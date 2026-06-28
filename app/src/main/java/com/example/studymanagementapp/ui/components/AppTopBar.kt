620415c855b4769c1369e3f48553792d2dff0226
Nicolas Klein <100135702+Nicolas-Klein@users.noreply.github.com>
2026-06-28 21:44:08 +0200

    fixed landscape configuration of topbar

--- app/src/main/java/com/example/studymanagementapp/ui/components/AppTopBar.kt
+++ app/src/main/java/com/example/studymanagementapp/ui/components/AppTopBar.kt
@@ -1,22 +1,30 @@
 package com.example.studymanagementapp.ui.components
 
+import android.content.res.Configuration
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SharedTopBar(screen: String) {
-    TopAppBar(
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.primary,
-        ),
-        title = {
-            Text(screen)
-        }
-    )
+
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+    if (!isLandscape) {
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            title = {
+                Text(screen)
+            }
+        )
+    }
 }
